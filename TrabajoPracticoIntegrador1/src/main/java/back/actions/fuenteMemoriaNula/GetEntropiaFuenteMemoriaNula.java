8dad6bf169a8de53f7f843daa75bbe2064ef5242
Luciano <luchyfran97@gmail.com>
2020-11-14 11:52:57 -0300

    se arreglo la fuente de memoria nula y el calculo de entropia en markov

--- TrabajoPracticoIntegrador1/src/main/java/back/actions/fuenteMemoriaNula/GetEntropiaFuenteMemoriaNula.java
+++ TrabajoPracticoIntegrador1/src/main/java/back/actions/fuenteMemoriaNula/GetEntropiaFuenteMemoriaNula.java
@@ -2,26 +2,38 @@ package back.actions.fuenteMemoriaNula;
 
 import back.actions.Actionable;
 import back.domain.calculadoras.CalculadoraCantInformacion;
+import back.domain.calculadoras.CalculadoraDeSimulacionFuenteNula;
 import back.domain.calculadoras.CalculadoraEntropia;
 import back.domain.formatters.FuenteMemoriaNulaResultFormatter;
 
 import java.util.ArrayList;
 
-public class GetEntropiaFuenteMemoriaNula implements Actionable  {
-    private final ArrayList<Double> probabilidades;
-    private final CalculadoraCantInformacion calculadoraCantInformacion = new CalculadoraCantInformacion();
-    private final CalculadoraEntropia calculadoraEntropia = new CalculadoraEntropia();
-    private final FuenteMemoriaNulaResultFormatter formatter = new FuenteMemoriaNulaResultFormatter();
+public class GetEntropiaFuenteMemoriaNula implements Actionable {
+	private final ArrayList<Double> probabilidades;
+	private final CalculadoraCantInformacion calculadoraCantInformacion = new CalculadoraCantInformacion();
+	private final CalculadoraEntropia calculadoraEntropia = new CalculadoraEntropia();
+	private final FuenteMemoriaNulaResultFormatter formatter = new FuenteMemoriaNulaResultFormatter();
+	private final CalculadoraDeSimulacionFuenteNula calculadoraSimulacion = new CalculadoraDeSimulacionFuenteNula();
 
-    public GetEntropiaFuenteMemoriaNula(ArrayList<Double> probabilidades) {
-        this.probabilidades = probabilidades;
-    }
+	public GetEntropiaFuenteMemoriaNula(ArrayList<Double> probabilidades) {
+		this.probabilidades = probabilidades;
+	}
 
-    @Override
-    public String execute() {
-        ArrayList<Double> infosIndividuales = calculadoraCantInformacion.invoke(probabilidades);
-        ArrayList<Double> entropiasIndividuales = calculadoraEntropia.calculaEntropiaIndividual(probabilidades, infosIndividuales);
-        double entropia = calculadoraEntropia.calculaEntropia(entropiasIndividuales);
-        return formatter.format(probabilidades, infosIndividuales, entropiasIndividuales, entropia);
-    }
+	@Override
+	public String execute() {
+		String simulacion = calculadoraSimulacion.invoke(probabilidades, 1000);
+		ArrayList<Integer> aparicionesSimulacion = calculadoraSimulacion.invoke(simulacion, probabilidades.size());
+		ArrayList<Double> probabilidadesSimuladas = calculadoraSimulacion.invoke(aparicionesSimulacion);
+		ArrayList<Double> infosIndividuales = calculadoraCantInformacion.invoke(probabilidades);
+		ArrayList<Double> infosIndividualesSimuladas = calculadoraCantInformacion.invoke(probabilidadesSimuladas);
+		ArrayList<Double> entropiasIndividuales = calculadoraEntropia.calculaEntropiaIndividual(probabilidades,
+				infosIndividuales);
+		ArrayList<Double> entropiasIndividualesSimuladas = calculadoraEntropia
+				.calculaEntropiaIndividual(probabilidadesSimuladas, infosIndividualesSimuladas);
+		double entropia = calculadoraEntropia.calculaEntropia(entropiasIndividuales);
+		double entropiaSimulada = calculadoraEntropia.calculaEntropia(entropiasIndividualesSimuladas);
+		return formatter.format(probabilidades, infosIndividuales, entropiasIndividuales, entropia, simulacion,
+				probabilidadesSimuladas, aparicionesSimulacion, infosIndividualesSimuladas,
+				entropiasIndividualesSimuladas, entropiaSimulada);
+	}
 }
--- TrabajoPracticoIntegrador1/src/main/java/back/domain/formatters/FuenteMemoriaNulaResultFormatter.java
+++ TrabajoPracticoIntegrador1/src/main/java/back/domain/formatters/FuenteMemoriaNulaResultFormatter.java
@@ -3,40 +3,68 @@ package back.domain.formatters;
 import java.util.ArrayList;
 
 public class FuenteMemoriaNulaResultFormatter {
-    public String format(ArrayList<Double> probabilidades, ArrayList<Double> infoIndividual,
-                         ArrayList<Double> entropiaIndividual, double entropia) {
-        return getInfoProbabilidades(probabilidades) + getInformacionIndividual(infoIndividual) +
-                getInfoEntropiaIndividual(entropiaIndividual) + getInfoEntropia(entropia);
-    }
 
-    private String getInfoProbabilidades(ArrayList<Double> probabilidades) {
-        String infoProbabilidades = "P(Si)";
-        int i = 0;
-        for (Double x: probabilidades) {
-            infoProbabilidades += "\nP(S"+ ++i +") = " + x;
-        }
-        return infoProbabilidades + "\n\n";
-    }
+	public String format(ArrayList<Double> probabilidades, ArrayList<Double> infoIndividual,
+			ArrayList<Double> entropiaIndividual, double entropia, String simulacion,
+			ArrayList<Double> probabilidadesSimuladas, ArrayList<Integer> aparicionesSimulacion,
+			ArrayList<Double> infosSimuladas, ArrayList<Double> entropiasIndividualesSimuladas,
+			double entropiaSimulada) {
 
-    private String getInformacionIndividual(ArrayList<Double> informacionesIndividuales) {
-        int i=0;
-        String infoIndividual = "I(Si)";
-        for (Double x: informacionesIndividuales) {
-            infoIndividual += "\nI(S"+ ++i +") = "+String.format("%1.7f", x) + " binits";
-        }
-        return infoIndividual + "\n\n";
-    }
+		return getInfoSimulacion(simulacion, aparicionesSimulacion)
+				+ getInfoProbabilidades(probabilidades, probabilidadesSimuladas)
+				+ getInformacionIndividual(infoIndividual, infosSimuladas)
+				+ getInfoEntropiaIndividual(entropiaIndividual, entropiasIndividualesSimuladas)
+				+ getInfoEntropia(entropia, entropiaSimulada);
+	}
 
-    private String getInfoEntropiaIndividual(ArrayList<Double> entropiaIndividual) {
-        int i=0;
-        String infoEntropia = "H(Si)";
-        for (Double x: entropiaIndividual) {
-            infoEntropia += "\nH(S"+ ++i +") = "+String.format("%1.7f", x) + " binits/simbolo";
-        }
-        return infoEntropia + "\n";
-    }
+	private String getInfoSimulacion(String simulacion, ArrayList<Integer> aparicionesSimulacion) {
+		char simbolo = 'a';
+		String infoSimulacion = "Simulacion realizada para un N=1000 con las probabilidades ingresadas:\n" + simulacion + "\n\n";
+		infoSimulacion += "Simbolo | Apariciones en la simulacion\n";
+		for (int i = 0; i < aparicionesSimulacion.size(); i++)
+			infoSimulacion += "   " + simbolo++ + "           | " + aparicionesSimulacion.get(i) + "\n";
+		return infoSimulacion + "\n";
+	}
 
-    private String getInfoEntropia(double entropia) {
-        return "\nH(S) = "+ entropia + " binits/simbolo \n\n";
-    }
+	private String getInfoProbabilidades(ArrayList<Double> probabilidades, ArrayList<Double> probabilidadesSimuladas) {
+		String infoProbabilidades = "Simbolos y probabilidades: \nSimbolo | Probabilidad Ingresada | Probabilidad Simulada";
+		char simbolo = 'a';
+		for (int i = 0; i < probabilidades.size(); i++) {
+			infoProbabilidades += "\n   " + simbolo++ + "           | " + probabilidades.get(i) + "\t\t    | "
+					+ probabilidadesSimuladas.get(i);
+		}
+		return infoProbabilidades + "\n\n";
+	}
+
+	private String getInformacionIndividual(ArrayList<Double> informacionesIndividuales,
+			ArrayList<Double> infosSimuladas) {
+		char simbolo = 'a';
+		String infoIndividual = "Información en [binits] que transmite cada simbolo:";
+		infoIndividual += "\nSimbolo | Informacion con datos ingresados | Informacion con datos simulados";
+		for (int i = 0; i < informacionesIndividuales.size(); i++) {
+			infoIndividual += "\n   " + simbolo++ + "           | "
+					+ String.format("%1.7f", informacionesIndividuales.get(i)) + " \t                       | "
+					+ String.format("%1.7f", infosSimuladas.get(i));
+		}
+		return infoIndividual + "\n\n";
+	}
+
+	private String getInfoEntropiaIndividual(ArrayList<Double> entropiaIndividual,ArrayList<Double> entropiasSimuladas) {
+		char simbolo = 'a';
+		String infoEntropia = "Entropia de cada simbolo en [binits/simbolo] :";
+		infoEntropia += "\nSimbolo | Entropia con datos ingresados | Entropia con datos simulados";
+		for (int i = 0; i < entropiaIndividual.size(); i++) {
+			infoEntropia += "\n   " + simbolo++ + "           | "
+					+ String.format("%1.7f", entropiaIndividual.get(i)) + " \t                 | "
+					+ String.format("%1.7f", entropiasSimuladas.get(i));
+		}
+		return infoEntropia + "\n\n";
+	}
+
+	private String getInfoEntropia(double entropia,double entropiaSimulada) {
+		String infoEntropia = "Entropia de la fuente en [binits/simbolo] :";
+		infoEntropia += "\nCon datos ingresados : H(S)= " + entropia;
+		infoEntropia += "\nCon datos simulados  : H(S)= " + entropiaSimulada;
+		return infoEntropia + "\n\n";
+	}
 }
